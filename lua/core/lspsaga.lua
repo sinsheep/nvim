15c19179e1e6dfbfc3e4689ade7b51dbc9c6b1d4
yangshixin <yangshixin@imdada.cn>
2022-08-20 18:00:53 +0800

    add comment

--- lua/core/lspsaga.lua
+++ lua/core/lspsaga.lua
@@ -12,5 +12,24 @@ vim.keymap.set("v", "<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>", { s
 local saga = require("lspsaga")
 
 saga.init_lsp_saga({
-    -- your configuration
+    symbol_in_winbar = {
+        in_custom = true
+    }
 })
+
+
+local term = require("lspsaga.floaterm")
+
+-- float terminal also you can pass the cli command in open_float_terminal function
+vim.keymap.set("n", "<leader>lg", function()
+    term.open_float_terminal("lazygit")
+end, { silent = true })
+vim.keymap.set("t", "<C-c>", function()
+    vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true))
+    term.close_float_terminal()
+end, { silent = true })
+
+-- or use command
+-- vim.keymap.set("n", "<A-d>", "<cmd>Lspsaga open_floaterm custom_cli_command<CR>", { silent = true })
+-- vim.keymap.set("t", "<A-d>", "<C-\\><C-n><cmd>Lspsaga close_floaterm<CR>", { silent = true })
+
--- lua/keymap/init.lua
+++ lua/keymap/init.lua
@@ -1,34 +1,44 @@
 require('keymap.config')
 local key = require('core.keymap')
-local nmap = key.nmap
+local nmap, vmap = key.nmap, key.vmap
 local silent, noremap = key.silent, key.noremap
 local opts = key.new_opts
 local cmd = key.cmd
 
 -- usage of plugins
 nmap({
-  -- packer
-  { '<Leader>pu', cmd('PackerUpdate'), opts(noremap, silent) },
-  { '<Leader>pi', cmd('PackerInstall'), opts(noremap, silent) },
-  { '<Leader>pc', cmd('PackerCompile'), opts(noremap, silent) },
-  -- dashboard { '<Leader>n', cmd('DashboardNewFile'), opts(noremap, silent) }, { '<Leader>ss', cmd('SessionSave'), opts(noremap, silent) }, { '<Leader>sl', cmd('SessionLoad'), opts(noremap, silent) },
-  -- nvimtree
-  { '<Leader>e', cmd('NvimTreeToggle'), opts(noremap, silent) },
-  -- Telescope
-  { '<Leader>fb', cmd('Telescope buffers'), opts(noremap, silent) },
-  { '<Leader>fg', cmd('Telescope live_grep'), opts(noremap, silent) },
-  { '<Leader>ff', cmd('Telescope find_files'), opts(noremap, silent) },
-  { '<Leader>fh', cmd('Telescope help_tags'), opts(noremap, silent) },
-  { '<leader>fo', cmd('Telescope oldfiles'), opts(noremap, silent)},
-  -- toggleterm
-  { '<leader>lg', cmd('lua _LAZYGIT_TOGGLE()'), opts(noremap, silent)},
-  { '<leader>py', cmd('lua _PYTHON_TOGGLE()'), opts(noremap, silent)},
+    -- packer
+    { '<Leader>pu', cmd('PackerUpdate'), opts(noremap, silent) },
+    { '<Leader>pi', cmd('PackerInstall'), opts(noremap, silent) },
+    { '<Leader>pc', cmd('PackerCompile'), opts(noremap, silent) },
+    -- dashboard { '<Leader>n', cmd('DashboardNewFile'), opts(noremap, silent) }, { '<Leader>ss', cmd('SessionSave'), opts(noremap, silent) }, { '<Leader>sl', cmd('SessionLoad'), opts(noremap, silent) },
+    -- nvimtree
+    { '<Leader>e', cmd('NvimTreeToggle'), opts(noremap, silent) },
+    -- Telescope
+    { '<Leader>fb', cmd('Telescope buffers'), opts(noremap, silent) },
+    { '<Leader>fg', cmd('Telescope live_grep'), opts(noremap, silent) },
+    { '<Leader>ff', cmd('Telescope find_files'), opts(noremap, silent) },
+    { '<Leader>fh', cmd('Telescope help_tags'), opts(noremap, silent) },
+    { '<leader>fo', cmd('Telescope oldfiles'), opts(noremap, silent)},
+    -- toggleterm
+    { '<leader>lg', cmd('lua _LAZYGIT_TOGGLE()'), opts(noremap, silent)},
+    { '<leader>py', cmd('lua _PYTHON_TOGGLE()'), opts(noremap, silent)},
 
 
-  -- diagnostic
-  {'<leader>g', vim.diagnostic.open_float, opts(noremap,silent)},
-  -- code_runnign
+    -- diagnostic
+    {'<leader>g', vim.diagnostic.open_float, opts(noremap,silent)},
+    -- code_runnign
 
-  {'<leader>R', cmd("RunCode"), opts(noremap, silent)}
+    {'<leader>R', cmd("RunCode"), opts(noremap, silent)},
+    {'<leader>C', cmd("RunClose"),opts(noremap,silent)},
+
+    -- lspsaga
+    {"<leader>rn", "<cmd>Lspsaga rename<CR>",opts(noremap, silent)},
+    {"<leader>ca", "<cmd>Lspsaga code_action<CR>",opts(noremap, silent)},
+    {"gh", "<cmd>Lspsaga lsp_finder<CR>", opts(noremap,silent) },
+    {"gp", "<cmd>Lspsaga preview_definition<CR>",opts(noremap,silent)},
+    {"<leader>cd", cmd('Lspsaga show_line_diagnostics'),opts(noremap, silent)},
+    {'<leader>tg', cmd('LSoutlineToggle'), opts(noremap, silent)}
 })
 
+vmap({"<leader>ca", "<cmd><C-U>Lspsaga range_code_action<CR>", opts(noremap,silent)})
